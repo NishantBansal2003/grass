9a5e8156d49b02a29635b6b1eefdd7cd339d5188
Nishant Bansal <nishant.bansal.282003@gmail.com>
2025-06-19 23:37:46 +0530

    fix failing tests
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- general/g.list/tests/conftest.py
+++ general/g.list/tests/conftest.py
@@ -4,46 +4,52 @@ import pytest
 
 
 @pytest.fixture
-def simple_dataset(tmp_path_factory):
-    """Set up a GRASS session for the tests."""
+def setup_session(tmp_path_factory, monkeypatch):
+    """Creates a session with a mapset"""
     tmp_path = tmp_path_factory.mktemp("simple_dataset")
-    project = "test_project"
-    mapset = "test_1"
-
-    # Create a test project
-    gs.create_project(tmp_path, project)
-
-    # Initialize the GRASS session
-    with gs.setup.init(tmp_path / project, env=os.environ.copy()) as session:
-        gs.run_command("g.region", rows=3, cols=3, env=session.env)
-
-        # Create Mock Mapsets and data in each
-        gs.run_command(
-            "g.mapset", project=project, mapset=mapset, flags="c", env=session.env
-        )
-
-        # Create a raster in this mapset
-        gs.mapcalc(f"raster_{mapset} = int(row())", env=session.env)
-        gs.run_command(
-            "r.support",
-            map=f"raster_{mapset}",
-            title=f"Raster title {mapset}",
-            env=session.env,
-        )
-
-        # Create a vector in this mapset
-        gs.run_command(
-            "v.mkgrid",
-            map=f"vector_{mapset}",
-            grid=[10, 10],
-            type="point",
-            env=session.env,
-        )
-        gs.run_command(
-            "v.support",
-            map=f"vector_{mapset}",
-            map_name=f"Vector title {mapset}",
-            env=session.env,
-        )
-
+    location = "test_project"
+    gs.core._create_location_xy(tmp_path, location)  # pylint: disable=protected-access
+    with gs.setup.init(tmp_path / location, env=os.environ.copy()) as session:
+        for key, value in session.env.items():
+            monkeypatch.setenv(key, value)
         yield session
+
+
+@pytest.fixture
+def simple_dataset(setup_session):
+    """Set up a GRASS session for the tests."""
+    mapset = "test_1"
+    project = "test_project"
+
+    gs.run_command("g.region", rows=3, cols=3, env=setup_session.env)
+
+    # Create Mock Mapsets and data in each
+    gs.run_command(
+        "g.mapset", project=project, mapset=mapset, flags="c", env=setup_session.env
+    )
+
+    # Create a raster in this mapset
+    gs.mapcalc(f"raster_{mapset} = int(row())", env=setup_session.env)
+    gs.run_command(
+        "r.support",
+        map=f"raster_{mapset}",
+        title=f"Raster title {mapset}",
+        env=setup_session.env,
+    )
+
+    # Create a vector in this mapset
+    gs.run_command(
+        "v.mkgrid",
+        map=f"vector_{mapset}",
+        grid=[10, 10],
+        type="point",
+        env=setup_session.env,
+    )
+    gs.run_command(
+        "v.support",
+        map=f"vector_{mapset}",
+        map_name=f"Vector title {mapset}",
+        env=setup_session.env,
+    )
+
+    yield setup_session
