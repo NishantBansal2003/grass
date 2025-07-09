d7656d00042b66b25a25ef08275fd2f1a27753b0
Nishant Bansal <nishant.bansal.282003@gmail.com>
2025-07-09 13:02:52 +0530

    db.connect: add JSON support
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- db/db.connect/testsuite/test_db_connect.py
+++ db/db.connect/testsuite/test_db_connect.py
@@ -1,6 +1,6 @@
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
-from grass.script.core import read_command
+from grass.script.core import read_command, parse_command
 
 
 class TestDbConnect(TestCase):
@@ -16,6 +16,13 @@ class TestDbConnect(TestCase):
         self.assertIn("schema: ", result)
         self.assertIn("group: ", result)
 
+        # Repeat with explicit plain format
+        result = read_command("db.connect", flags="p", format="plain")
+        self.assertIn("driver: ", result)
+        self.assertIn("database: ", result)
+        self.assertIn("schema: ", result)
+        self.assertIn("group: ", result)
+
     def test_db_connect_shell(self):
         """Test shell-format output"""
         result = read_command("db.connect", flags="g")
@@ -23,6 +30,27 @@ class TestDbConnect(TestCase):
         self.assertIn("database=", result)
         self.assertIn("schema=", result)
         self.assertIn("group=", result)
+
+        # Repeat with explicit shell format
+        result = read_command(
+            "db.connect",
+            flags="p",
+            format="shell",
+        )
+        self.assertIn("driver=", result)
+        self.assertIn("database=", result)
+        self.assertIn("schema=", result)
+        self.assertIn("group=", result)
+
+    def test_db_connect_json(self):
+        """Test JSON output"""
+        result = parse_command(
+            "db.connect",
+            flags="p",
+            format="json",
+        )
+        expected_keys = ["driver", "database", "schema", "group"]
+        self.assertCountEqual(list(result.keys()), expected_keys)
 
 
 if __name__ == "__main__":
